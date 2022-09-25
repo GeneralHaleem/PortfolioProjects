9814374508dc2111a0f125ab59083022d6d09bf7
Salami Aleem <salami31032000@gmail.com>
2022-09-25 21:36:47 +0100

    Add files via upload

---
+++ BellaBeat Data CHANGE LOG.sql
@@ -0,0 +1,26 @@
+#Null Values
+#1. I removed the "fat" column from the weighloginfo dataset because it only had two values, rendering it unsuitable for my research.
+#2. Using Excel, I utilized the filter tools to look for null values across the dataset. There weren't any.
+
+#Duplicate Values
+#1. Throughout the dataset, I checked for values that were duplicated. They didn't exist.
+
+#Structural Errors.
+#1. Checked for structural errors in the datsets, they weren't any.
+#2. By changing all of the values to 2d.p, I ensured that all of the numeric values are consistent.
+
+#Parse Date
+#1. To ensure that all dates were consistent, I formatted the date column to read YYYY-MM-DD.
+
+# Populating the dataset
+#1. From the date columns, a column for the day of the week was established.
+#2. From the date columns, a column for the month was also created.
+
+#
+#1. In the datetime columns, time is separated from date.
+
+#Validation
+# I checked at the conclusion of the data cleaning procedure to see if the data:
+#. madke sense?
+#. follow the relevant rules for its field?
+#. substantiate or invalidate my working idea, or reveal any new information?
